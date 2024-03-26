4f9009bf31012baa0491689d74005fa0b186944d
Munir <smsirajulmonir@gmail.com>
2024-03-26 10:19:40 +0600

    navbar ready

---
+++ src/Main/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Components/Header/Header";
+
+const Main = () => {
+    return (
+        <div className="max-w-6xl mx-auto mt-5">
+            <Header></Header>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
